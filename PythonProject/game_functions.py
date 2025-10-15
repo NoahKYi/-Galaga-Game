67960bd33911ae7b0aa03ac063d995257deee970
NoahKYi <156741902+NoahKYi@users.noreply.github.com>
2025-10-14 20:31:55 -0700

    Ship works...
    Deleted refactored code.

--- PythonProject/game_functions.py
+++ PythonProject/game_functions.py
@@ -1,15 +1,30 @@
 import sys
 import pygame
 
+
+"'Allows the player to move the ship using event listeners'"
 def check_events(ship):
     for event in pygame.event.get():
-        if event.type == pygame.event.get():
+        if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                #The event key listens for the key to push it right
-                ship.rect.centerx += 21
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
 
+#These events are for the keydown events
+def check_keydown_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+def check_keyup_events(event,ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+#This is for updating the class
 def update_screen(ai_settings, screen, ship):
     screen.fill(ai_settings.bg_color)
     ship.blitme()
--- PythonProject/ship.py
+++ PythonProject/ship.py
@@ -3,9 +3,9 @@ import pygame
 class Ship():
 #Pygame treats things like rectangles
 #x,y coordinates & Origin are the top left of the rectangle
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         self.screen = screen
-
+        self.ai_settings = ai_settings
         self.image = pygame.image.load("PythonGalagaShip.bmp")
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
@@ -15,7 +15,24 @@ class Ship():
         self.rect.bottom = self.screen_rect.bottom
         #Defines where the ship is at
 
+        self.center = float(self.rect.centerx)
+        #Decimal for the ship's center
+
+        self.moving_right = False
+        self.moving_left = False
+        #Movement Flags
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        self.rect.centerx = self.center
+        #Update rect object from the ship's center
+
     def blitme(self):
         #Draws ship at its location
         self.screen.blit(self.image, self.rect)
+
 
